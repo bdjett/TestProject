725dbd92aab2c0fb1516bb53ae47b60963043070
Brian Jett <brian.jett@disney.com>
2016-09-29 18:19:11 -0700

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Jett, Brian on 9/29/16.
+//  Copyright © 2016 Brian Jett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
